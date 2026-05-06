1eb1a14e4b4070d32ea9dff26d1637af02139006
Chetan Sharma <chetansharma53700@gmail.com>
2026-05-06 13:36:35 +0530

    Update app.py

--- app.py
+++ app.py
@@ -30,3 +30,38 @@ if st.button("Generate Email"):
 
     else:
         st.warning("Please enter topic")
+
+from langchain_core.prompts import PromptTemplate
+from langchain_openai import ChatOpenAI
+from templates import TEMPLATES
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+def generate_email(topic, tone):
+
+    template = TEMPLATES[tone]
+
+    prompt = PromptTemplate(
+        input_variables=["topic"],
+        template=template
+    )
+
+    llm = ChatOpenAI(
+        model="deepseek/deepseek-chat-v3-0324:free",
+
+        base_url="https://openrouter.ai/api/v1",
+
+        api_key=os.getenv("OPENROUTER_API_KEY"),
+
+        temperature=0.7
+    )
+
+    chain = prompt | llm
+
+    response = chain.invoke({
+        "topic": topic
+    })
+
+    return response.content
